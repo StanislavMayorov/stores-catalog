c2cb519ab09304af94371e3a71b231a3e0a6c63a
Stanislav <Stanislav@fksis.by>
2016-11-17 20:02:46 +0300

    add ShopDetailComponent routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,18 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ShopListComponent} from "./shop-list/shop-list.component";
+import { ShopListComponent } from "./shop-list/shop-list.component";
+import { ShopDetailComponent } from "./shop-detail/shop-detail.component";
 
 
 const appRoutes: Routes = <Routes>[
   {
     path: '',
     component: ShopListComponent
+  },
+  {
+    path: 'shop/:serialNumber',
+    component: ShopDetailComponent
   }
 ];
-
-
-
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
