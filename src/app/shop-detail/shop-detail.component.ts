49b629e71939490ab01f6e099dc340e0227d1ecd
Stanislav <Stanislav@fksis.by>
2016-11-18 11:50:27 +0300

    add shop-detail layout

--- src/app/shop-detail/shop-detail.component.ts
+++ src/app/shop-detail/shop-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from "@angular/router";
+import { ShopService } from "../shared/shope.service";
+import { Shop } from "../shared/shop";
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-shop-detail',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop-detail.component.sass']
 })
 export class ShopDetailComponent implements OnInit {
+  shop: Shop;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private shopService: ShopService) {
+  }
 
   ngOnInit() {
+    this.route.params
+      .switchMap((params: Params) => params['serialNumber'])
+      .subscribe((serialNumber: string) =>
+        this.shop = this.shopService.getShopBySerialNumber(Number(serialNumber)));
+  }
+
+  editHandler(serialNumber: number) {
+    //this.shop.updateProduct();
+  }
+
+  deleteHandler(serialNumber: number) {
+    this.shop.deleteProduct(serialNumber);
   }
 
 }
