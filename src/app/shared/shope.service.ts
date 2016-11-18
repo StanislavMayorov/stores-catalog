dfd7e7f978e0ec994a28d03f127e558749923f91
Stanislav <Stanislav@fksis.by>
2016-11-19 00:14:58 +0300

    fix mock shops

--- src/app/shared/shope.service.ts
+++ src/app/shared/shope.service.ts
@@ -32,21 +32,21 @@ export class ShopService {
   }
 
   private setMock() {
-    var startOpeningHoursKST = new Time(8, 0);
-    var finishOpeningHoursKST = new Time(20, 0);
-    var shopKST = new Shop('KST', startOpeningHoursKST,
+    let startOpeningHoursKST = new Time(8, 0);
+    let finishOpeningHoursKST = new Time(20, 0);
+    let shopKST = new Shop('KST', startOpeningHoursKST,
       finishOpeningHoursKST, 'Dekabristov 5, Minsk, Belarus');
-    var product1 = new Product('Termopasta', 'Eheu, amor!Olla moris, tanquam flavum byssus.' +
+    let product1 = new Product('Termopasta', 'Eheu, amor!Olla moris, tanquam flavum byssus.' +
       ' Heu, secundus habena!Cum visus experimentum, omnes elogiumes examinare velox, germanus lamiaes.');
-    var product2 = new Product('HDD Toshiba', 'Devatio undas, tanquam teres gemna.');
+    let product2 = new Product('HDD Toshiba', 'Devatio undas, tanquam teres gemna.');
     shopKST.addProduct(product1);
     shopKST.addProduct(product2);
     this.setShop(shopKST);
 
-    var startOpeningHoursBatareiki = new Time(9, 0);
-    var finishOpeningHoursBatareiki = new Time(18, 0);
-    var shopBatareiki = new Shop('batareiki.by', startOpeningHoursBatareiki,
-      finishOpeningHoursBatareiki, ' Cullman 15, Minsk, Belarus');
+    let startOpeningHoursBatareiki = new Time(9, 0);
+    let finishOpeningHoursBatareiki = new Time(18, 0);
+    let shopBatareiki = new Shop('batareiki.by', startOpeningHoursBatareiki,
+      finishOpeningHoursBatareiki, 'Cullman 15, Minsk, Belarus');
     product1 = new Product('DURACELL AA 2500', 'Cur ignigena potus?Cursus de castus brodium, examinare epos!');
     product2 = new Product('PANASONIC AAA ENELOOP', 'Cum cotta trabem, omnes spatiies resuscitabo raptus, brevis zetaes.');
     shopBatareiki.addProduct(product1);
