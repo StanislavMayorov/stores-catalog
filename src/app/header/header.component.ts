9c7fa455b1049432967a7160c6bbb21d1000adf4
Stanislav <Stanislav@fksis.by>
2016-11-22 14:03:07 +0300

    update navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: []
+  styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
 
